268c7976750b7049a8a8cc38c385dbce77e3dc05
josh <joshwbrook01@gmail.com>
2023-09-29 15:43:01 +0200

    clean up repo, keep only essentials

--- TEST_CMA.py
+++ TEST_CMA.py
@@ -1,15 +1,10 @@
-import sys
+import os
+import cma
+import time
+import numpy as np
 
 from evoman.environment import Environment
 from demo_controller import player_controller
-
-# imports other libs
-import time
-import numpy as np
-from math import fabs,sqrt
-import glob, os
-import cma
-import matplotlib.pyplot as plt
 
 
 headless = True
@@ -17,29 +12,31 @@ if headless:
     os.environ["SDL_VIDEODRIVER"] = "dummy"
 
 
-experiment_name = 'cma_opt_test'
+experiment_name = "cma_opt_test"
 if not os.path.exists(experiment_name):
     os.makedirs(experiment_name)
 
 n_hidden_neurons = 10
-env = Environment(experiment_name=experiment_name,
-                  enemies=[8],
-                  playermode="ai",
-                  player_controller=player_controller(n_hidden_neurons),
-                  enemymode="static",
-                  level=2,
-                  speed="fastest",
-                  visuals=False)
+env = Environment(
+    experiment_name=experiment_name,
+    enemies=[1],
+    playermode="ai",
+    player_controller=player_controller(n_hidden_neurons),
+    enemymode="static",
+    level=2,
+    speed="fastest",
+    visuals=False,
+)
 
 ini = time.time()  # sets time marker
 
 
 # genetic algorithm params
 
-run_mode = 'train' # train or test
+run_mode = "train"  # train or test
 
 # number of weights for multilayer with 10 hidden neurons
-n_vars = (env.get_num_sensors()+1)*n_hidden_neurons + (n_hidden_neurons+1)*5
+n_vars = (env.get_num_sensors() + 1) * n_hidden_neurons + (n_hidden_neurons + 1) * 5
 
 
 dom_u = 1
@@ -49,40 +46,38 @@ gens = 30
 mutation = 0.2
 last_best = 0
 
-def simulation(env,x):
-    f,p,e,t = env.play(pcont=x)
-    return abs(f*-1)
+
+def simulation(env, x):
+    f, p, e, t = env.play(pcont=x)
+    return abs(f * -1)
 
 
+# n_vars = (env.get_num_sensors()+1)*n_hidden_neurons + (n_hidden_neurons+1)*5
 
-#n_vars = (env.get_num_sensors()+1)*n_hidden_neurons + (n_hidden_neurons+1)*5
-
-#100 dif weights
+# 100 dif weights
 pop = np.random.uniform(dom_l, dom_u, (npop, n_vars))
 best_weights = []
 
 
-for i in range(0,99):
-
-    #select pop iter
+for i in range(0, 99):
+    # select pop iter
     cur_pop = pop[i]
-    #init cma
-    es = cma.CMAEvolutionStrategy(cur_pop, 40)
-    #es.opts.set({'tolflatfitness' : 5})
-    es.opts.set({'ftarget': 2.5})
 
-    #find solution for current weights
+    # init cma
+    es = cma.CMAEvolutionStrategy(cur_pop, 40)
+
+    # es.opts.set({'tolflatfitness' : 5})
+    es.opts.set({"ftarget": 2.5})
+
+    # find solution for current weights
     while not es.stop():
         solutions = es.ask()
-        es.tell(solutions,[simulation(env, cur_pop) for cur_pop in solutions])
+        es.tell(solutions, [simulation(env, cur_pop) for cur_pop in solutions])
         es.logger.add()
         es.disp()
 
-# SAVE PLOTS & BEST WEIGHTS
-    #es.logger.plot()
-    #plt.savefig('evo_plot.png')
-    #append the current best weight to best_weights list
+    # append the current best weight to best_weights list
     best_weights.append(es.result_pretty()[0].copy())
-    #np.savetxt('best_x___||', best_weights, delimiter=',')
 
-np.savetxt('best_x___||', best_weights, delimiter=',')
+# SAVE WEIGHTS
+np.savetxt("best_x___||", best_weights, delimiter=",")
---
+++ test_own_EA.py
@@ -0,0 +1,56 @@
+import os
+import numpy as np
+
+from evoman.environment import Environment
+from demo_controller import player_controller
+
+
+experiment_name = "weight_test"
+if not os.path.exists(experiment_name):
+    os.makedirs(experiment_name)
+
+n_hidden_neurons = 10
+
+
+# initializes simulation in individual evolution mode, for single static enemy.
+env = Environment(
+    experiment_name=experiment_name,
+    enemies=[2],
+    playermode="ai",
+    player_controller=player_controller(n_hidden_neurons),
+    speed="normal",
+    enemymode="static",
+    level=2,
+    visuals=True,
+)
+
+
+# TESTING WEIGHTS OBTAINED FROM OWN EA
+# MAKE SURE THE FILENAME FOR WEIGHTS AND FITNESS CORRESPONDS TO THE WEIGHTS AND FIT YOU WANT TO TEST
+# SEE BELOW TO TEST WEIGHTS FROM CMA
+with open("output/pop_level1_try1") as f:
+    pop = f.readlines()
+
+for i in range(len(pop)):
+    pop[i] = pop[i].split(",")
+    for j in range(len(pop[i])):
+        pop[i][j] = pop[i][j].strip()
+        pop[i][j] = float(pop[i][j])
+
+
+with open("output/fit_level1_try1") as f:
+    fit = f.readlines()
+
+for i in range(len(fit)):
+    fit[i] = fit[i].split(",")
+    for j in range(len(fit[i])):
+        fit[i][j] = fit[i][j].strip()
+        fit[i][j] = float(fit[i][j])
+
+
+max_i = np.argmax(fit)
+pop = pop[max_i]
+
+
+f, _, _, _ = env.play(pcont=np.array(pop))
+print("Fitness: ", f)
